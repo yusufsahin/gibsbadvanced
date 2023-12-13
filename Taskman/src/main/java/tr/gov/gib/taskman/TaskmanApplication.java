4842a996b9019c567e721c652b637d979c7870e2
Yusuf Sahin <yusuf@yusufsahin.net>
2023-12-13 11:31:00 +0300

    Docker Cammand & Spring Boot App created

---
+++ Taskman/src/main/java/tr/gov/gib/taskman/TaskmanApplication.java
@@ -0,0 +1,13 @@
+package tr.gov.gib.taskman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmanApplication.class, args);
+    }
+
+}
---
+++ Taskman/src/test/java/tr/gov/gib/taskman/TaskmanApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.gov.gib.taskman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
