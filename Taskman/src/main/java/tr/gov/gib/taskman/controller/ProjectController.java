eec7c8203159f14955c3eb11962a3fd62614a8a4
Yusuf Sahin <yusuf@yusufsahin.net>
2023-12-15 11:54:10 +0300

    Update ProjectController

--- Taskman/src/main/java/tr/gov/gib/taskman/controller/ProjectController.java
+++ Taskman/src/main/java/tr/gov/gib/taskman/controller/ProjectController.java
@@ -2,7 +2,9 @@ package tr.gov.gib.taskman.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import tr.gov.gib.taskman.dto.ProjectDto;
 import tr.gov.gib.taskman.dto.WorkitemDto;
@@ -10,6 +12,7 @@ import tr.gov.gib.taskman.service.ProjectService;
 import tr.gov.gib.taskman.service.WorkitemService;
 import tr.gov.gib.taskman.util.ApiPath;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,41 +23,71 @@ public class ProjectController {
 
     @Autowired
     public ProjectController(ProjectService projectService) {
+
         this.projectService = projectService;
     }
-
+    @PreAuthorize("hasAuthority('READ_PRIVILEGE')")
     @GetMapping
     public ResponseEntity<List<ProjectDto>> getAllWorkitems()
     {
         List<ProjectDto> data= projectService.getProjects();
         return ResponseEntity.ok(data);
     }
-
+    @PreAuthorize("hasAuthority('READ_PRIVILEGE')")
     @GetMapping("/{id}")
-    public ResponseEntity<ProjectDto> getProjectById(@PathVariable Long id)
+    public ResponseEntity<ProjectDto> getProjectById(@PathVariable(value = "projectid", required = true) Long projectid)
     {
-        ProjectDto projectDto= projectService.getByProjectId(id);
-        return ResponseEntity.ok(projectDto);
-    }
 
+        try {
+            ProjectDto projectDto= projectService.getByProjectId(projectid);
+            return ResponseEntity.ok(projectDto);
+        }catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null); //return 404, with null body
+        }
+
+
+    }
+    @PreAuthorize("hasAuthority('WRITE_PRIVILEGE')")
     @PostMapping
     public ResponseEntity<ProjectDto> createProject(@Valid @RequestBody ProjectDto projectDto)
     {
+        try
+        {
         ProjectDto newProject= projectService.create(projectDto);
-        return ResponseEntity.ok(newProject);
-    }
+        return ResponseEntity.created(new URI(ApiPath.ProjectCtrl.CTRL+"/"+newProject.getId())).body(newProject);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
 
+    }
+    @PreAuthorize("hasAuthority('WRITE_PRIVILEGE')")
     @PutMapping("/{id}")
     public ResponseEntity<ProjectDto> updateProject(@PathVariable Long id,@Valid @RequestBody ProjectDto projectDto)
     {
-        ProjectDto updatedProject= projectService.update(projectDto);
-        return ResponseEntity.ok(updatedProject);
+        try {
+            projectService.update(projectDto);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProject(@PathVariable Long id)
-    {
-        projectService.delete(id);
-        return ResponseEntity.noContent().build();
+    { try {
+        if(id!=null)
+        {
+
+            projectService.delete(id);
+            return ResponseEntity.noContent().build();
+        }
+        else
+        {
+            return ResponseEntity.badRequest().build();
+        }
+    } catch (Exception e) {
+        return ResponseEntity.notFound().build();
+    }
     }
 }
