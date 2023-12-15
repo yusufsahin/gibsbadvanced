1111dbc80274d7768c681f9c098d8946c0fcc667
Yusuf Sahin <yusuf@yusufsahin.net>
2023-12-15 15:03:02 +0300

    NoteAPI and Mongo Docker

---
+++ NoteAPI/src/main/java/tr/gov/gib/noteapi/NoteApiApplication.java
@@ -0,0 +1,13 @@
+package tr.gov.gib.noteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteApiApplication.class, args);
+    }
+
+}
---
+++ NoteAPI/src/test/java/tr/gov/gib/noteapi/NoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.gov.gib.noteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
